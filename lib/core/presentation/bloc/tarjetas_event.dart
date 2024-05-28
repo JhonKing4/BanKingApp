2fbf2f397d1e528dc7e8dc7ba80c27abce5da834
CrystianACR <crystianescuela14@gmail.com>
2024-05-27 19:34:53 -0500

    bloc2

---
+++ lib/core/presentation/bloc/tarjetas_event.dart
@@ -0,0 +1,41 @@
+import 'package:equatable/equatable.dart';
+
+abstract class TarjetasEvent extends Equatable {
+  const TarjetasEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class LoadFormDataEvent extends TarjetasEvent {}
+
+class TarjetaPChanged extends TarjetasEvent {
+  final String tarjeta_pic;
+
+  const TarjetaPChanged(this.tarjeta_pic);
+
+  @override
+  List<Object?> get props => [tarjeta_pic];
+}
+
+class SaldoChanged extends TarjetasEvent {
+  final String saldo_tarjeta;
+
+  const SaldoChanged(this.saldo_tarjeta);
+
+  @override
+  List<Object?> get props => [saldo_tarjeta];
+}
+
+class NumeroTChanged extends TarjetasEvent {
+  final String numero_tarjeta;
+
+  const NumeroTChanged(this.numero_tarjeta);
+
+  @override
+  List<Object?> get props => [numero_tarjeta];
+}
+
+
+
+class FormSubmitted extends TarjetasEvent {}
