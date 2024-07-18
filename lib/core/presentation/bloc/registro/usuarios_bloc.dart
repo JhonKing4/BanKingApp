c64a17a683f2110ec5635cfa0fb154f7a684863f
LeonardoDRebollo <leonardod.rebollo@gmail.com>
2024-07-18 06:44:00 -0500

    menu

--- lib/core/presentation/bloc/registro/usuarios_bloc.dart
+++ lib/core/presentation/bloc/registro/usuarios_bloc.dart
@@ -1,10 +1,10 @@
 import 'package:bankingapp/core/presentation/bloc/registro/usuarios_event.dart';
 import 'package:bankingapp/core/presentation/bloc/registro/usuarios_state.dart';
-import 'package:bankingapp/core/presentation/screens/data/domain/usecases/load_usuarios_data.dart';
+import 'package:bankingapp/core/presentation/screens/data/domain/usecases/load_usuarios_data.dart' as usecase;
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
-  final SubmitRegister submitRegister;
+  final usecase.SubmitRegister submitRegister;
 
   RegisterBloc(this.submitRegister) : super(RegisterInitial()) {
     on<SubmitRegisterEvent>((event, emit) async {
